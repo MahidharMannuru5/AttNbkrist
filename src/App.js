216b668d5e166f2c58911b830ac48dc574910aa9
MahidharMannuru5 <20kb1a0581@nbkrist.org>
2022-07-04 21:36:00 +0530

    fxing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Attendance from './Pages/Attendance';
 import MidMarks from './Pages/MidMarks';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import {HashRouter as Router,Routes,Route} from "react-router-dom"
 
 function App() {
   return (
