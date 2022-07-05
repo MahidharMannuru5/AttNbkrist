77ac1eabc3b951665d9727f9c5313e781fac6870
MahidharMannuru5 <20kb1a0581@nbkrist.org>
2022-07-05 11:28:15 +0530

    index

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import Attendance from './Pages/Attendance';
 import MidMarks from './Pages/MidMarks';
-import {HashRouter as Router,Routes,Route} from "react-router-dom"
+import {HashRouter as Router,Routes,Route,Link} from "react-router-dom"
 
 function App() {
   return (
     <>
     <div className="App">
-    
+
     <div className="container-fluid">
       <a href="/">Attendance</a>
       <a href="/#/MidMarks">MidMarks</a>
       
       </div>
       <br/>
-    </div>
 
- 
+    
+    </div>
+      
         <Routes>
         <Route path="/" element={<Attendance/>}/>
         <Route path="/MidMarks" element={<MidMarks/>}/>
