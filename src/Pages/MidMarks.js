174a75b6d6594cc4b7e18c56f48fe32db69d79fe
MahidharMannuru5 <20kb1a0581@nbkrist.org>
2022-07-06 15:52:54 +0530

    new

--- src/Pages/MidMarks.js
+++ src/Pages/MidMarks.js
@@ -116,6 +116,12 @@ export default function MidMarks() {
     <div className='footer'>
       <h3>Trust The Process,Time has a Reply</h3>
     </div>
+    <div className="help">
+    <a href="https://www.hitwebcounter.com">
+<img className="counter"src="https://hitwebcounter.com/counter/counter.php?page=8007496&style=0036&nbdigits=7&type=page&initCount=0" title="Free Counter" Alt="web counter"   border="0" /></a>                                    
+                                    
+    
+    </div>
     </>
   );
 }
