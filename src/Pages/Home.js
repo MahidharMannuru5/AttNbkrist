8bd4a9b0b3594f71069a7698f535204e0ba3a91b
Mahidhar Mannuru <81514511+MahidharMannuru5@users.noreply.github.com>
2023-01-20 13:24:30 +0000

    AddingHomeOfBlog

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className="Blog-post">
+      <h3>Introduction To Programming</h3>
+      <p>Programming is the process of creating instructions that a computer can understand and execute. It involves writing code in a specific programming language, such as Python or JavaScript, and using that code to create software, apps, and other digital products.
+         It is a versatile and in-demand skill that can be used in a wide range of industries.</p>
+      <h5>@MahidharMannuru</h5>
+      </div>
+    </>
   )
 }
 
