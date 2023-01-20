54dd5473aae7ed08120a23e70863bab832ec259f
Mahidhar Mannuru <81514511+MahidharMannuru5@users.noreply.github.com>
2023-01-20 14:16:35 +0000

    FixedIssueWithFireStoreFetchData

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,39 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import {getDocs,collection} from "firebase/firestore"
+import {db} from "../ConfigFirebase/Firebase"
+import {useState} from "react"
 const Home = () => {
+  const[contentpost,setcontentpost]=useState([]);
+  const collectionReference=collection(db,"ContentPosts");
+  useEffect(()=>{
+    const getPosts=async()=>{
+      const docRef=await getDocs(collectionReference);
+      const data=docRef.docs.map((doc)=>doc.data());
+      setcontentpost(data);
+      console.log(data);
+    }
+    getPosts();
+  },[])
+
   return (
     <>
-    <div className="Blog-post">
-      <h3>Introduction To Programming</h3>
-      <p>Programming is the process of creating instructions that a computer can understand and execute. It involves writing code in a specific programming language, such as Python or JavaScript, and using that code to create software, apps, and other digital products.
-         It is a versatile and in-demand skill that can be used in a wide range of industries.</p>
-      <h5>@MahidharMannuru</h5>
-      </div>
-    </>
-  )
+  
+    
+    {contentpost.map((post) => (
+    
+  <div key={post.id}>
+      <div className="Blog-post">
+    <h3>{post.Title}</h3>
+    <p>{post.Body}</p>
+    <h5>@MahidharMannuru</h5>
+  </div>
+  
+  </div>
+))}
+
+   </>
+       )
+ 
 }
 
 export default Home
