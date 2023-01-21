e426f64df997d5340b7afa1739bce561f2c9b642
Mahidhar Mannuru <81514511+MahidharMannuru5@users.noreply.github.com>
2023-01-21 06:13:24 +0000

    AddedSigInFeatureWithPasswordLessVerification

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,67 +1,43 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import {useState} from 'react';
-import {  sendSignInLinkToEmail } from "firebase/auth";
-import {auth} from '../ConfigFirebase/Firebase';
+import { useState } from 'react';
+import { auth } from '../ConfigFirebase/Firebase';
+import { sendSignInLinkToEmail } from "firebase/auth";
+
 const actionCodeSettings = {
-  // URL you want to redirect back to. The domain (www.example.com) for this
-  // URL must be in the authorized domains list in the Firebase Console.
-  url: 'https://mahidharmannuru5-legendary-bassoon-4r9x74g9g65f7gqj-3000.preview.app.github.dev/AttNbkrist?#/',
-  // This must be true.
-  handleCodeInApp: true,
-  iOS: {
-    bundleId: 'com.example.ios'
-  },
-  android: {
-    packageName: 'com.example.android',
-    installApp: true,
-    minimumVersion: '12'
-  },
-  dynamicLinkDomain: 'example.page.link'
+  url: 'https://mahidharmannuru5.github.io/AttNbkrist/',
+  handleCodeInApp: true
 };
+
 const Signin = () => {
-  const[Email,setEmail]=useState(" ");
-  const Login=async()=>{
-
-    await auth.sendSignInLinkToEmail(Email, actionCodeSettings)
-
-    .then(() => {
-    // The link was successfully sent. Inform the user.
-    // Save the email locally so you don't need to ask the user for it again
-    // if they open the link on the same device.
-    window.localStorage.setItem('emailForSignIn', Email);
-
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ...
-  });
+  const [Email, setEmail] = useState("");
+  const Login = async () => {
+    await sendSignInLinkToEmail(auth, Email, actionCodeSettings)
+      .then(() => {
+        console.log("Email sent.")
+      })
+      .catch((error) => {
+        console.log(error)
+      });
   }
+
   return (
     <>
-    <div className="container">
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" onChange={(event)=>setEmail(event.target.value)} placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      
-      <Button variant="primary" type="submit" onClick={Login}>
-        Login
-      </Button>      
-    </Form>
-    </div>
+      <div className="container">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" onChange={(event) => setEmail(event.target.value)} placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={Login}>
+            Login
+          </Button>
+        </Form>
+      </div>
     </>
   )
 }
