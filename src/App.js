cabe558b4f3684bb4c37cb3d34ac480bf604b124
Mahidhar Mannuru <81514511+MahidharMannuru5@users.noreply.github.com>
2023-01-23 01:44:28 +0000

    AddingFirebaseRouteProtection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Attendance from './Pages/Attendance';
+import {auth} from "./ConfigFirebase/Firebase"
+import { onAuthStateChanged } from "firebase/auth";
+import {useState} from "react"
 import MidMarks from './Pages/MidMarks';
 import SignUp from './Pages/SignUp';
 import Signin from './Pages/Signin';
@@ -9,14 +12,31 @@ import MakeAcall from './Pages/MakeAcall';
 import ChatSystem from './Pages/ChatSystem';
 import {Routes,Route,Link} from "react-router-dom"
 import NavBar from './Components/NavBar';
+import { useEffect } from 'react';
 
 function App() {
+  const[uid,setUid]=useState("Nothing")
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log(uid)
+        setUid(auth.user.displayName);
+          
+  
+      } else {
+        console.log("no user")
+          setUid("no user")
+      }
+    });
+    
+  }, [])
   return (
     <>
 
     <div className="App">
     <NavBar/>
-     
+     {uid}
   </div>
  <br/>
           <div ClassName="container-f">
--- src/ConfigFirebase/Firebase.js
+++ src/ConfigFirebase/Firebase.js
@@ -1,4 +1,4 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword,onAuthStateChanged } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
